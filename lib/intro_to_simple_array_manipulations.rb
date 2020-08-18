d55dd37b7a9a5e24ad8385450d0d533d1f05831d
Zack Carlson <jzcarlson014@gmail.com>
2020-08-18 16:44:51 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(colors_in_the_rainbow, next_color)
+  colors_in_the_rainbow.push next_color
+end
+
+def using_unshift(bourough_in_nyc, new_neighborhood)
+  bourough_in_nyc.unshift(new_neighborhood)
+end
+
+def using_pop(continents)
+  continents.pop
+end
+
+def pop_with_args(dog_breeds)
+  dog_breeds.pop(2)
+end
+
+def using_shift(my_favorite_cities)
+  my_favorite_cities.shift
+end
+
+def shift_with_args(ice_cream_brands)
+  ice_cream_brands.shift(2)
+end
