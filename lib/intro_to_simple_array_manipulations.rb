16fe7abb8cebdf0e09dbb5ab6abe8fdf80e01176
Jeffrey Bolling <j2b2590@gmail.com>
2020-08-25 21:33:02 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,25 @@
+
+
+def using_push (array, arg)
+  array.push(arg)
+end
+
+def using_unshift(array, str)
+  array.unshift(str)
+end
+
+def using_pop(array)
+  array.pop
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.shift
+end
+
+def shift_with_args(array)
+  array.shift(2)
+end
